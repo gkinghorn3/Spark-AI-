6e25b2c8b45c180c38395a9cd3e6d8565aaa4f7d
Grant <gkinghorn3@icloud.com>
2023-12-01 14:47:34 +0000

    react markdown package added and configured to format responses from the API

--- spark/app/(dashboard)/(routes)/code/page.tsx
+++ spark/app/(dashboard)/(routes)/code/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import axios from "axios";
 import * as z from "zod";
@@ -16,30 +16,28 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
-// import { ChatCompletionMessage } from "openai/resources/chat/index.mjs";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+import OpenAI from "openai";
 
-import Empty from "@/components/empty"
-import Loader from "@/components/loader"
+import ReactMarkdown from "react-markdown";
+
+import Empty from "@/components/empty";
+import Loader from "@/components/loader";
 import UserAvatar from "@/components/user-avatar";
 import BotAvatar from "@/components/bot-avatar";
-import OpenAI from "openai";
-import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
-
-
-
 
 export default function CodePage() {
-  const router = useRouter()
-  const [messages, setMessages] = useState<OpenAI.Chat.ChatCompletionMessageParam[]>([]);
-
+  const router = useRouter();
+  const [messages, setMessages] = useState<
+    OpenAI.Chat.ChatCompletionMessageParam[]
+  >([]);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       prompt: "",
-
-    }
-  })
+    },
+  });
 
   const isLoading = form.formState.isSubmitting;
 
@@ -48,23 +46,23 @@ export default function CodePage() {
       const userMessage: ChatCompletionMessageParam = {
         role: "user",
         content: values.prompt,
-      } 
-      const newMessages = [...messages, userMessage]
+      };
+      const newMessages = [...messages, userMessage];
 
-      const response = await axios.post('/api/code', {
+      const response = await axios.post("/api/code", {
         messages: newMessages,
       });
 
-      setMessages((current) => [...current, userMessage, response.data])
+      setMessages((current) => [...current, userMessage, response.data]);
 
-      form.reset()
+      form.reset();
     } catch (error: any) {
-        // TODO: open pro Modal
-        console.log(error)
+      // TODO: open pro Modal
+      console.log(error);
     } finally {
-      router.refresh()
+      router.refresh();
     }
-  }
+  };
 
   return (
     <div>
@@ -78,36 +76,38 @@ export default function CodePage() {
       <div className="px-4 lg:px-8">
         <div>
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}
-            className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm
              grid grid-cols-12 gap-2"
             >
-            <FormField 
-             name='prompt'
-             render={({field}) => (
-              <FormItem className="col-span-12 lg:col-span-10">
-                <FormControl className='m-0 p-0'>
-                  <Input 
-                  className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
-                  disabled={isLoading}
-                  placeholder="Create a simple React toggle button"
-                  {...field}
-                  />
-
-                </FormControl>
-              </FormItem>
-             )}
-            />
-            <Button className="col-span-12 lg:col-span-2 w-full" disabled={isLoading}>
-              Generate
-            </Button >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Create a simple React toggle button"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
             </form>
-
-          </Form >
+          </Form>
           <div className="space-7-4 mt-4">
             {isLoading && (
               <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
-                  <Loader />
+                <Loader />
               </div>
             )}
             {messages.length === 0 && !isLoading && (
@@ -115,22 +115,34 @@ export default function CodePage() {
             )}
             <div className="flex flex-col-reverse gap-y-4">
               {messages.map((message) => (
-                <div key={message.content}
-                className={cn('p-8 w-full flex items-start gap-x-8 rounded-lg',
-                message.role === "user" ? 'bg-white border border-black/10' : 'bg-muted' 
-                )}
+                <div
+                  key={message.content}
+                  className={cn(
+                    "p-8 w-full flex items-start gap-x-8 rounded-lg",
+                    message.role === "user"
+                      ? "bg-white border border-black/10"
+                      : "bg-muted"
+                  )}
                 >
-                  {message.role === "user" ? <UserAvatar /> :  <BotAvatar />}
-                  <p className='text-sm'>
-                  {message.content}
+                  {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+                  <ReactMarkdown
+                  components={{
 
-                  </p>
-          
+                    pre: ({ node, ...props }) => (
+                      <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                        <pre {...props} />
+                      </div>
+                    ), 
+                    code: ({ node, ...props }) => (
+                      <code className="bg-black/10 rounded-lg p-1" {...props} />
+                    )
+                  }}
+                  className='text-sm overflow-hidden leading-7'
+                  >{message.content || ""}</ReactMarkdown>
                 </div>
               ))}
             </div>
           </div>
-
         </div>
       </div>
     </div>
