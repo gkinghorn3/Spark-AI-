0de1bbfe86f0f953e59a6154aadbf9344211a50e
Grant <gkinghorn3@icloud.com>
2023-12-01 10:06:05 +0000

    user-avatar component changed- original code wasnt fetching profile user image

--- spark/components/user-avatar.tsx
+++ spark/components/user-avatar.tsx
@@ -4,15 +4,26 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function UserAvatar() {
 
-    const user = useUser();
-  return (
+  const { isSignedIn, user, isLoaded } = useUser();
+
+   
+  if (!isLoaded) {
+    return null;
+  }
+
+  if (isSignedIn) {
+    return (
     
-    <Avatar>
-      <AvatarImage src={user?.imageUrl} />
-      <AvatarFallback>
-        {user?.firstName?.charAt(0)}
-        {user?.lastName?.charAt(0)}
-      </AvatarFallback>
-    </Avatar>
-  );
+      <Avatar>
+        <AvatarImage src={user?.imageUrl} />
+        <AvatarFallback>
+          {user?.firstName?.charAt(0)}
+          {user?.lastName?.charAt(0)}
+          
+        </AvatarFallback>
+      </Avatar>
+    );
+
+  } 
+  
 }
